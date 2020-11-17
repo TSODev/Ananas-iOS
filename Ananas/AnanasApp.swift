3eaff306daf99b3de7ef9b80b39337e1508cd414
Thierry Soulie <thierry@tsodev.fr>
2020-11-17 15:41:47 +0100

    Initial Commit

---
+++ Ananas/AnanasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnanasApp.swift
+//  Ananas
+//
+//  Created by Thierry Soulie on 17/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct AnanasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
